3c200cb7dcb7bec340b17a35473978e04ff22cdd
nschutz <nschutz@student.42wolfsburg.de>
2024-04-03 15:49:16 +0200

    CPP08 feat: ex01 first version

---
+++ ex01/Span.cpp
@@ -0,0 +1,74 @@
+#include "Span.hpp"
+
+Span::Span(unsigned int N) : N(N) {
+}
+
+Span::Span(const Span &other) {
+    *this = other;
+}
+
+Span &Span::operator=(const Span &other) {
+    if (this != &other) {
+        N = other.N;
+        numbers = other.numbers;
+    }
+    return *this;
+}
+
+Span::~Span() {
+}
+
+void Spann::addNumber(int number)
+{
+    if (numbers.size() >= N)
+        throw std::exception();
+    numbers.push_back(number);
+}
+
+int Span::shortestSpan()
+{
+    int i, l;
+    int j = numbers[0] - numbers[1];
+    int k = 0;
+
+    if (numbers.size() <= 1)
+        throw std::exception();
+    while (k < numbers.size())
+    {
+        l = k + 1;
+        while (l < numbers.size()) {
+            if (numbers[k] > numbers[l])
+                i = numbers[k] - numbers[l];
+            else if (numbers[l] > numbers[k])
+                i = numbers[l] - numbers[k];
+            else
+                throw std::exception();
+            if (i < j)
+                j = i;
+            l++;
+        }
+        k++;
+    }
+    return (j);
+}
+
+int Span::longestSpan()
+{
+    int i = numbers.begin();
+    int j = numbers.begin();
+    int k = 1;
+
+    if (numbers.size() <= 1)
+        throw std::exception();
+    while (k < numbers.size())
+    {
+        if (i < numbers[k])
+            i = numbers[k];
+        if (j > numbers[k])
+            j = numbers[k];
+        k++;
+    }
+    if (i == j)
+        throw std::exception();
+    return (j - i);
+}
---
+++ ex01/Span.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+class Span
+{
+public:
+    Span(unsigned int N);
+    Span(const Span &other);
+    Span &operator=(const Span &other);
+    ~Span();
+
+    void addNumber(int number);
+    int shortestSpan();
+    int longestSpan();
+
+private:
+    unsigned int N;
+    std::list<int> numbers;
+};
